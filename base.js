ff140cfd7d57f87e7cf264a2b039ad060a52a65e
isangieri <isabelleangieri@gmail.com>
2015-12-01 09:31:57 -0800

    Fixed a bug with the gifsearch input

--- base.js
+++ base.js
@@ -1,34 +1,29 @@
 $(document).ready(function(){
-	$("#gifsearch").keydown(function(event) {
-		if (event.which === 13) {
-			console.log($("#gifsearch").val);
-	}
-
-	var searchText = $("#gifsearch").val();
 
 	function convertStringToSearchTerm(string) {
 	    string = string.replace(/ /g, "+");
 	    return string;
 	}
+	
+	$("#gifsearch").keydown(function(event) {
+		if (event.which === 13) {
+			$("#gifsearch").html("");
 
-	convertStringToSearchTerm(searchText);
+			var searchText = $("#gifsearch").val();
 
-	$.ajax({
-		method: 'GET',
-		url: 'http://api.giphy.com/v1/gifs/search?q=' + searchText + '&api_key=dc6zaTOxFJmzC',
-		dataType: 'json',
-		success: function (taco) {
-			taco.data.forEach(function(element,index){
-				$(".gif").append("<img src='" + taco.data[index].images.fixed_height.url + "'>");
+			convertStringToSearchTerm(searchText);
+
+			$.ajax({
+				method: 'GET',
+				url: 'http://api.giphy.com/v1/gifs/search?q=' + searchText + '&api_key=dc6zaTOxFJmzC',
+				dataType: 'json',
+				success: function (taco) {
+					taco.data.forEach(function(element,index){
+						$(".gif").append("<img src='" + taco.data[index].images.fixed_height.url + "'>");
+					});
+		  		}
 			});
-  	}
+		}
 	});
-	});
-
-	// $('#searchgif').submit(function(x) {
- //    x.preventDefault();
- //    var submit = this.submit();
- //    console.log(submit);
-	// });
 
 }); 
