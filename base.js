d864f8a1adea50e2b0b5e81ba76f495d27d204fe
isangieri <isabelleangieri@gmail.com>
2015-11-30 22:11:48 -0800

    completed basic requirements and began the challenge to use user input within the ajax request.

--- base.js
+++ base.js
@@ -1,7 +1,29 @@
 $(document).ready(function(){
-  console.log("here's your mock data to start with:")
-  console.log(GLOBAL_MOCK_DATA_OBJECT)
 
-  // can you render the mock data to the page?
+	var searchText = "drake rap";
 
-})
+	function convertStringToSearchTerm(string) {
+	    string = string.replace(/ /g, "+");
+	    return string;
+	}
+
+	convertStringToSearchTerm(searchText);
+
+	$.ajax({
+		method: 'GET',
+		url: 'http://api.giphy.com/v1/gifs/search?q=' + searchText + '&api_key=dc6zaTOxFJmzC',
+		dataType: 'json',
+		success: function (taco) {
+			taco.data.forEach(function(element,index){
+				$(".gif").append("<img src='" + taco.data[index].images.fixed_height.url + "'>");
+			});
+  	}
+	});
+
+	// $('#searchgif').submit(function(x) {
+ //    x.preventDefault();
+ //    var submit = this.submit();
+ //    console.log(submit);
+	// });
+
+}); 
